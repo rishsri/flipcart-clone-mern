917168348251d0b7f166f2d98f4980f7d324561d
Rishabh Srivastava <rishabhsri36@gmail.com>
2021-06-16 11:53:18 +0530

    Added a functionality in Login Page

--- Clinet/src/component/login/loginDialog.jsx
+++ Clinet/src/component/login/loginDialog.jsx
@@ -11,8 +11,8 @@ import {
 
 const useStyle = makeStyles({
   component: {
-    height: "70vh",
-    width: "90vh",
+    height: "83vh",
+    width: "100vh",
     maxWidth: "unset !important",
   },
   image: {
@@ -20,7 +20,7 @@ const useStyle = makeStyles({
     background: "#2874f0",
     backgroundPosition: "center 85%",
     backgroundRepeat: "no-repeat",
-    height: "70vh",
+    height: "83vh",
     width: "40%",
     padding: "45px 35px",
     "& > *": {
@@ -43,6 +43,10 @@ const useStyle = makeStyles({
     color: "#fff",
     height: 48,
     borderRadius: 2,
+    "&:hover": {
+      backgroundColor: "#FB641B",
+      color: "#fff",
+    },
   },
   requestbtn: {
     textTransform: "none",
@@ -51,6 +55,10 @@ const useStyle = makeStyles({
     height: 48,
     borderRadius: 2,
     boxShadow: "0 2px 4px 0 rgb(0 0 0 / 20%)",
+    "&:hover": {
+      backgroundColor: "#fff",
+      color: "#2874f0",
+    },
   },
   text: {
     color: "#878787",
@@ -73,11 +81,35 @@ const useStyle = makeStyles({
   },
 });
 
-const LoginDialog = ({ open, setOpen, setAccount }) => {
+const initialValue = {
+  login: {
+      view: 'login',
+      heading: 'Login',
+      subHeading: " Get access to your Orders, Wishlist and Recommendations"
+  },
+  signup: {
+    view: 'signup',
+    heading: "Looks like you're new here",
+    subHeading: 'Signup to get started'
+}
+}
+
+const LoginDialog = ({ open, setOpen }) => {
   const classes = useStyle();
+
+  const [account, setAccount] = useState(initialValue.login);
+
+  const toggleAccountFunction = () => {
+    setAccount(initialValue.signup)
+  }
+
+  const  toggleAccountLoginFunction = () => {
+    setAccount(initialValue.login)
+  }
 
   const handleClose = () => {
     setOpen(false);
+    setAccount(initialValue.login)
   };
 
   return (
@@ -85,30 +117,44 @@ const LoginDialog = ({ open, setOpen, setAccount }) => {
       <DialogContent className={classes.component}>
         <Box style={{ display: "flex" }}>
           <Box className={classes.image}>
-            <Typography variant="h5">Login</Typography>
-            <Typography style={{ marginTop: 20 }}>
-              Get access to your Orders, Wishlist and Recommendations
+            <Typography variant="h5"> {account.heading} </Typography>
+            <Typography style={{ marginTop: 20, color: "#D3D3D3" }}>
+              {account.subHeading}
             </Typography>
           </Box>
-          <Box className={classes.login}>
-            <TextField name="username" label="Enter Email/Mobile number" />
-            <TextField name="password" label="Enter Password" />
-            <Typography className={classes.text}>
-              By continuing, you agree to Flipkart's Terms of Use and Privacy
-              Policy.
-            </Typography>
-            <Button className={classes.loginbtn}>Login</Button>
-            <Typography
-              className={classes.text}
-              style={{ textAlign: "center" }}
-            >
-              OR
-            </Typography>
-            <Button className={classes.requestbtn}>Request OTP</Button>
-            <Typography className={classes.createText}>
-              New to Flipkart? Create an account
-            </Typography>
-          </Box>
+
+          {account.view === "login" ? (
+            <Box className={classes.login}>
+              <TextField name="username" label="Enter Email/Mobile number" />
+              <TextField name="password" label="Enter Password" />
+              <Typography className={classes.text}>
+                By continuing, you agree to Flipkart's Terms of Use and Privacy
+                Policy.
+              </Typography>
+              <Button className={classes.loginbtn}>Login</Button>
+              <Typography
+                className={classes.text}
+                style={{ textAlign: "center" }}
+              >
+                OR
+              </Typography>
+              <Button className={classes.requestbtn}>Request OTP</Button>
+              <Typography className={classes.createText} onClick  = {toggleAccountFunction}>
+                New to Flipkart? Create an account
+              </Typography>
+            </Box>
+          ) : (
+            <Box className={classes.login}>
+            <TextField  name='firstname' label='Enter Firstname' />
+            <TextField  name='lastname' label='Enter Lastname' />
+            <TextField  name='username' label='Enter Username' />
+            <TextField  name='email' label='Enter Email' />
+            <TextField  name='password' label='Enter Password' />
+            <TextField  name='phone' label='Enter Phone' />
+            <Button className={classes.loginbtn}>Continue</Button>
+            <Button className={classes.requestbtn} onClick = {toggleAccountLoginFunction}>Existing User? Log in </Button>
+        </Box>
+          )}
         </Box>
       </DialogContent>
     </Dialog>
